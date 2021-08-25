1758926443ba949cad79b89be880a88c48b0b5e6
aahzambardai <ahzam.bardai@outlook.com>
2021-08-25 11:53:26 -0500

    new resume

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // import fs from 'fs'
 const fs = require('fs')
 
-fs.readFile('./info.json', 'utf8', function (err, data) {
+fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
     if(err){
         console.log(err)
     } else {
