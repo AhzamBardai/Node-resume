e067982b4006d3641f3927a0960d83d12b7b9469
aahzambardai <ahzam.bardai@outlook.com>
2021-08-25 11:17:46 -0500

    resume

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+// import fs from 'fs'
+const fs = require('fs')
+
+fs.readFile('./info.json', 'utf8', function (err, data) {
+    if(err){
+        console.log(err)
+    } else {
+        console.log(data)
+        return data
+    }
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Welcome to my Resume
+This is a test app with my resume information made using Node.js.
